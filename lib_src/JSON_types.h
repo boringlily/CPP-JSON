0e3ee720bbf6457328883ebaaa80ce8d12059bda
Lily Nikitin <53998902+boringlily@users.noreply.github.com>
2024-09-08 16:14:45 -0700

    some changes and ideas.

--- lib_src/JSON_types.h
+++ lib_src/JSON_types.h
@@ -4,6 +4,28 @@
 class iToken
 {
  iToken() = delete;
- 
+}; 
+
+
+class Object: public iToken
+{
 
 };
+
+
+class VarArray : public iToken
+{
+
+}; 
+
+
+class ObjectArray : public iToken
+{
+
+}; 
+
+
+class ObjectArray : public iToken
+{
+
+}; 
--- main/main.cpp
+++ main/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include "JSON.hpp"
+
+
+
 
 int main()
 {
-    printf("THIS IS MAIN");
+    std::string my_json_string{"{ \"unsigned_var\": 8, \"signed_var\": -15, \"object\":{\"var1\"} }"};
+
+    CPP_JSON::Reader my_file();
+    printf("%s", my_json_string);
 
     return 0;
 }
