f5c19c747c67d6872b2e7b3bfe89471350e15eaf
Lily Nikitin <53998902+boringlily@users.noreply.github.com>
2024-09-08 18:28:55 -0700

    added example json file for parsing design and testing

--- main/main.cpp
+++ main/main.cpp
@@ -6,10 +6,21 @@
 
 int main()
 {
-    std::string my_json_string{"{ \"unsigned_var\": 8, \"signed_var\": -15, \"object\":{\"var1\"} }"};
+    FILE * JSON_FILE; 
+    char * file_buffer = static_cast<char *>(calloc(1024, 1));
+    JSON_FILE = fopen("/Users/lily/Documents/GitHub/CPP-JSON/main/example.json", "r");
+    if(! JSON_FILE)
+    {
+        printf("FUCK");
+        return -1;
+    }
+    
+    fread(file_buffer, 1024, 1, JSON_FILE);
+    std::string my_json_string{file_buffer};
+    free(file_buffer);
 
-    CPP_JSON::Reader my_file();
-    printf("%s", my_json_string);
+    // CPP_JSON::Reader my_file(my_json_string);
+    // printf("%s", my_json_string.c_str());
 
     return 0;
 }
